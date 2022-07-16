410e5941943db3389e8660b781f2a2509a822ca0
Ana-teston <anapaulateston@gmail.com>
2022-07-16 19:03:28 +0200

    add function for change the background

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log('testing');
+const { body } = document;
+
+function changeBackground(number) {
+  // Check if background alred showing
+  let previousBackground;
+  if (body.className) {
+    previousBackground = body.className;
+  }
+  // Reset css class fro body
+  body.className = '';
+  switch (number) {
+    case '1':
+      body.classList.add('background-1');
+      break;
+    case '2':
+      body.classList.add('background-2');
+      break;
+    case '3':
+      body.classList.add('background-3');
+      break;   
+  }
+}
