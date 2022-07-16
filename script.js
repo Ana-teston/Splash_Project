535395df790a08982f5868b227da0dbbec4fd07e
Ana-teston <anapaulateston@gmail.com>
2022-07-16 19:13:00 +0200

    switch function toggle buttons for change background

--- script.js
+++ script.js
@@ -10,13 +10,12 @@ function changeBackground(number) {
   body.className = '';
   switch (number) {
     case '1':
-      body.classList.add('background-1');
-      break;
+      return (previousBackground === 'background-1' ? false : body.classList.add('background-1'));
     case '2':
-      body.classList.add('background-2');
-      break;
+      return (previousBackground === 'background-2' ? false : body.classList.add('background-2'));
     case '3':
-      body.classList.add('background-3');
-      break;   
+      return (previousBackground === 'background-3' ? false : body.classList.add('background-3'));   
+    default:
+      break;  
   }
 }
